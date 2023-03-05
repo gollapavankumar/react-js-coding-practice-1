1e1b511919321df01add93770bf82646425c2504
root <gollapavankumar>
2023-03-05 17:30:49 +0000

    coding-practice-1

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <h1 className="heading">Super Over Leage</h1>
+    <div className="sub-container">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
